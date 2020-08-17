f4eb9ac84458fd50431caded790fe374b3589525
Junio MM <info@juniomarques.com>
2020-08-17 02:01:21 +0200

    init app test jest

---
+++ src/components/TechList/index.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+
+function TechList() {
+    const [techs, setTechs] = useState([]);
+    const [newTech, setNewTech] = useState('');
+
+    useEffect(() => {
+        const techs = localStorage.getItem('techs');
+
+        if(techs) {
+            setTechs(JSON.parse(techs));
+        }
+
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('techs', JSON.stringify(techs));
+    }, [techs]);
+
+    function handleAddTech() {
+        setTechs([...techs, newTech]);
+        setNewTech('');
+    }
+    return (
+            <form data-testid="tech-form" onSubmit={handleAddTech}>
+                <ul data-testid="tech-list">
+                    {techs.map(tech => <li key={tech}>{tech}</li>)}
+                </ul>
+                <label htmlFor="tech">Tech</label>
+                <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value) } type="text" />
+                <button>Adicionar</button>
+            </form>
+        );
+}
+
+export default TechList;
